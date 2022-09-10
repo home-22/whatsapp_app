0e1e1ec023fb0380d3008619649fe79154dbb87d
home-22 <armin.jasarevic.25@gmail.com>
2022-09-10 23:18:59 +0200

    Whatsapp_App

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp_App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          color: Colors.green
+        ),
+     
+      ),
+  
+    );
+  }
+}
