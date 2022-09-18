5ff6ba2cd711a74c6f3a4ec9586933a35d62a8ff
home-22 <armin.jasarevic.25@gmail.com>
2022-09-18 23:35:57 +0200

    whatsapp_app

--- lib/widgets/Status.dart
+++ lib/widgets/Status.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -32,18 +34,94 @@ class Status extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.only(left: 20),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: const [
                         Text(
                           'My Status',
                           style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Today, 12:30 am',
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black45),
                         ),
                       ],
                     ),
-                  )
+                  ),
+                  const Spacer(),
+                  Container(
+                    child: const Icon(
+                      Icons.more_vert,
+                      color: Colors.amber,
+                    ),
+                  ),
                 ],
               ),
             ),
+            const SizedBox(height: 10),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Recent Updates',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ),
+            for (int i = 1; i < 3; i++)
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 12),
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(2.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        border: Border.all(color: Colors.blue, width: 3),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(40),
+                        child: Image.asset(
+                          'images/user$i.png',
+                          height: 55,
+                          width: 55,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Alex  Herry',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Today, 1:40 am',
+                            style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black45),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
           ],
         ),
       ),
