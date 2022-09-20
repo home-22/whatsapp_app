11c797ad732a948f0eb87771f882bdcb3b754a29
home-22 <armin.jasarevic.25@gmail.com>
2022-09-21 01:36:38 +0200

    whatsapp_app

--- lib/widgets/Calls.dart
+++ lib/widgets/Calls.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -12,21 +13,122 @@ class Calls extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
         child: Column(
           children: [
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 12),
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(40),
-                    child: Image.asset(
-                      'images/user.png',
-                      height: 60,
-                      width: 60,
+            for (int i = 1; i < 4; i++)
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 12),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(40),
+                      child: Image.asset(
+                        'images/user$i.png',
+                        height: 60,
+                        width: 60,
+                      ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Dear',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Row(
+                            children: const [
+                              Icon(
+                                Icons.call_made,
+                                color: Colors.black45,
+                                size: 19,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                '(2) Today, 12:39',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black45,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    const Padding(
+                      padding: EdgeInsets.all(0),
+                      child: Icon(
+                        Icons.call_sharp,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+            for (int i = 1; i < 5; i++)
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 12),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(40),
+                      child: Image.asset(
+                        'images/user$i.png',
+                        height: 60,
+                        width: 60,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Dear',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Row(
+                            children: const [
+                              Icon(
+                                Icons.call_made,
+                                color: Colors.black45,
+                                size: 19,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                '(2) Today, 12:39',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black45,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    const Padding(
+                      padding: EdgeInsets.all(0),
+                      child: Icon(
+                        Icons.call_sharp,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
           ],
         ),
       ),
